de654cab0b2bf31c2afce1d546ed76b9ba944b8c
Dylan Burden <dylanburden@dylans-macbook-air.local>
2018-09-23 23:07:50 -0230

    Initial Commit

---
+++ ElementalRoguelite/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ElementalRoguelite
+//
+//  Created by Dylan Burden on 2018-09-23.
+//  Copyright © 2018 ElementalRoguelike. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
